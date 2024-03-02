c3be00111d540b72c48174bbd82b96ddde7171da
masatos1012 <masatosama1012.2@gmail.com>
2024-03-02 17:03:50 +0900

    認証用accountsアプリの追加

--- quiz1/url.py
+++ quiz1/url.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "quiz1"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+
 ]
--- quiz1/views.py
+++ quiz1/views.py
@@ -3,3 +3,5 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
